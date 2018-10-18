90e11779335bef9e0634cb08c2266f4bea78ca6b
Shamsutdinov Ruslan <kit4curs@gmail.com>
2018-10-18 21:51:18 +0500

    Initial commit

---
+++ app/src/main/java/com/example/rusli_000/webapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.rusli_000.webapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
