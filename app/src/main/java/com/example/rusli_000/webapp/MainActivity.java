51a20ed39dd347e7c59758c95ecc71f85f4e7fb4
Shamsutdinov Ruslan <kit4curs@gmail.com>
2018-10-20 16:01:16 +0500

    Test connect

--- app/src/main/java/com/example/rusli_000/webapp/MainActivity.java
+++ app/src/main/java/com/example/rusli_000/webapp/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.rusli_000.webapp;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button showBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        showBtn = (Button) findViewById(R.id.btnShow);
+        Log.d("TAGGG","ТЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕСССССССССТТТТ");
+    }
+    public void onButtonClick(View v)
+    {
+        switch (v.getId()) {
+            case R.id.btnShow:
+            {
+                Intent intent = new Intent(this,ShowActivity.class);
+                startActivity(intent);
+                break;
+            }
+
+        }
     }
 }
---
+++ app/src/main/java/com/example/rusli_000/webapp/ShowActivity.java
@@ -0,0 +1,78 @@
+package com.example.rusli_000.webapp;
+
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static android.util.Log.d;
+
+public class ShowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show);
+        String url = "http://localhost/webapps/get_all_product.php";
+        new LoadAllProducts().execute();
+    }
+
+
+}
+class LoadAllProducts extends AsyncTask<String,String,String> {
+    @Override
+    protected void onPreExecute()
+    {
+
+    }
+    @Override
+    protected String doInBackground(String... args) {
+        StringBuilder result = new StringBuilder();
+
+        HttpURLConnection urlConnection = null;
+        try {
+            URL url = new URL("https://ruslik2014.000webhostapp.com/get_all_product.php");
+            urlConnection = (HttpURLConnection) url.openConnection();
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                result.append(line);
+                Log.d("TAG",line);
+            }
+
+        }catch( Exception e) {
+            e.printStackTrace();
+            Log.d("TAG",e.getMessage());
+        }
+        finally {
+            urlConnection.disconnect();
+        }
+
+
+        return result.toString();
+    }
+    protected void onPostExecute(String result)
+    {
+
+    }
+}
