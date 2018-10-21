37806201e2fc4b7d6c737fb25bc98ca6a9735405
Shamsutdinov Ruslan <kit4curs@gmail.com>
2018-10-21 22:01:21 +0500

    Add products list

--- app/src/main/java/com/example/rusli_000/webapp/ShowActivity.java
+++ app/src/main/java/com/example/rusli_000/webapp/ShowActivity.java
@@ -5,6 +5,9 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -21,74 +24,93 @@ import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import static android.util.Log.d;
 
+
 public class ShowActivity extends AppCompatActivity {
 
+    static ArrayList<HashMap<String, String>> productsList;
+    ListView lvMain;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show);
         String url = "http://localhost/webapps/get_all_product.php";
+
+        productsList = new ArrayList<HashMap<String, String>>();
+        lvMain = (ListView) findViewById(R.id.list);
         new LoadAllProducts().execute();
+
+
     }
 
+    class LoadAllProducts extends AsyncTask<String,String,String> {
+        private String resultJSON="";
+        @Override
+        protected String doInBackground(String... args) {
+            StringBuilder result = new StringBuilder();
 
-}
-class LoadAllProducts extends AsyncTask<String,String,String> {
+            HttpURLConnection urlConnection = null;
+            try {
+                URL url = new URL("https://ruslik2014.000webhostapp.com/get_all_product.php");
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
 
-    private String resultJSON="";
-    @Override
-    protected String doInBackground(String... args) {
-        StringBuilder result = new StringBuilder();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
 
-        HttpURLConnection urlConnection = null;
-        try {
-            URL url = new URL("https://ruslik2014.000webhostapp.com/get_all_product.php");
-            urlConnection = (HttpURLConnection) url.openConnection();
-            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    result.append(line);
+                    Log.d("TAG",line);
+                }
+                resultJSON = result.toString();
 
-            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-
-            String line;
-            while ((line = reader.readLine()) != null) {
-                result.append(line);
-                Log.d("TAG",line);
+            }catch( Exception e) {
+                e.printStackTrace();
+                Log.d("TAG",e.getMessage());
             }
-            resultJSON = result.toString();
-
-        }catch( Exception e) {
-            e.printStackTrace();
-            Log.d("TAG",e.getMessage());
-        }
-        finally {
-            urlConnection.disconnect();
-        }
-
-
-        return resultJSON;
-    }
-    protected void onPostExecute(String result)
-    {
-        super.onPostExecute(result);
-        Log.d("postExecute", "COME IN");
-        try {
-            JSONObject jsonObject = new JSONObject(result);
-            JSONArray products = jsonObject.getJSONArray("products");
-            Log.d("JSON", " "+products.length());
-            for (int i = 0; i < products.length(); i++) {
-                JSONObject product = products.getJSONObject(i);
-                String id = product.getString("pid");
-                String name = product.getString("name");
-                String price = product.getString("price");
-                Log.d("JSON", id);
-                Log.d("JSON", name);
-                Log.d("JSON", price);
+            finally {
+                urlConnection.disconnect();
             }
-        }catch(JSONException e)
+
+
+            return resultJSON;
+        }
+        protected void onPostExecute(String result)
         {
-            Log.d("JSONException",e.getMessage());
+            super.onPostExecute(result);
+            Log.d("postExecute", "COME IN");
+
+            try {
+                JSONObject jsonObject = new JSONObject(result);
+                JSONArray products = jsonObject.getJSONArray("products");
+                Log.d("JSON", " "+products.length());
+                for (int i = 0; i < products.length(); i++) {
+                    JSONObject product = products.getJSONObject(i);
+                    String id = product.getString("pid");
+                    String name = product.getString("name");
+                    String price = product.getString("price");
+                    HashMap<String,String> map = new HashMap<String,String>();
+                    map.put("pid",id);
+                    map.put("name",name+"\n"+price);
+                    productsList.add(map);
+                    Log.d("JSON", id);
+                    Log.d("JSON", name);
+                    Log.d("JSON", price);
+                }
+            }catch(JSONException e)
+            {
+                Log.d("JSONException",e.getMessage());
+            }
+            ListAdapter adapter = new SimpleAdapter(
+                    ShowActivity.this,productsList,
+                    R.layout.list_item, new String[] {"pid","name"},
+                    new int[] {R.id.pid,R.id.name});
+            lvMain.setAdapter(adapter);
         }
     }
+
 }
